f13561f3279cadca8294aaeb5f913bd966f8004a
Hernan Ferreras <hernanferreras@icloud.com>
2024-03-14 16:42:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,21 @@ def mysplit(strng):
     #
     # coloca tu código aquí
     #
-    new = []
+    new = [] 
     word=''
-    strng = strng.strip()
+    strng = strng.strip() #elimina espacios en blanco al principio y al final de la cadena strng
     
-    if strng.isspace():
+    if strng.isspace(): #si strng es una cadena vacia devuelve una lista vacia
         return new
     
     for x in range (len(strng)):
         if strng[x]!= chr(32):
             word += strng[x]
-                
         else:
             new.append(word)
             word = ''
     
-    if word != '':
+    if word != '': #suma la ultima palabra a la lista new ya que al no tener espacio al final no puede incluirse en el bucle for
         new.append(word)
     
     return new
